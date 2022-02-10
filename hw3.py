c0dfa4d44aa52dd6487ff2101efc87c20e597c07
Zach Wadhams <33103466+zwadhams@users.noreply.github.com>
2022-02-10 15:53:42 -0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = np.matrix([['red', 'yes', 'north'], ['blue', 'no', 'south'],
+d = np.array([['red', 'yes', 'north'], ['blue', 'no', 'south'],
                ['yellow', 'no', 'east'], ['yellow', 'no', 'west'],
                ['red', 'yes', 'north'], ['yellow', 'yes', 'north'],
                ['blue', 'no', 'west']])
 
 print(d)
 
+X2 = d[:,1]
+countY = sum(X2 == 'yes')
+countN = sum(X2 == 'no')
 
-plt.bar(x = 0, height = 5, data = d)
-
+plt.bar(x = ('yes','no'), height = (countY, countN), color = ('blue', 'yellow'))
 plt.show()
+
